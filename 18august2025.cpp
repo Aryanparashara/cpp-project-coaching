ecc90d18d223e36ed520953dcec6d7bc92d15da9
Aryanparashara <aryanparashara01@gmail.com>
2025-08-21 21:41:31 +0530

    Create 18august2025.cpp

---
+++ 18august2025.cpp
@@ -0,0 +1,90 @@
+/*#include<iostream>
+int main()
+{
+  char c;
+  std::cout<<"\n enter a character ";
+  std::cin>>c;
+  if ((c>='a')&&(c<='z'))
+  {
+    std::cout<<"\n it is a small alphabet "<<c;
+  }
+  else if ((c>='A')&&(c<='Z'))
+  {
+    std::cout<<"\n it is a capital letter"<<c;
+  }
+  else if ((c>='0')&&(c<='9'))
+  {
+    std::cout<<"\n it is a number key "<<c;
+  }
+  else 
+  {
+    std::cout<<"\n it is a special key "<<c;
+  }
+}*/
+/*#include<iostream>
+int main()
+{
+  float fh,cl;
+  int choice;
+  std::cout<<"\n 1. convert temperarture from fahrenheit to celscius ";
+  std::cout<<"\n 2. convert temprature from celscius to fahrenheit ";
+  std::cout<<"\n enter your choice ";
+  std::cin>>choice;
+  if(choice==1)
+  {
+    std::cout<<"\n enter temprature in fahrenheit ";
+    std::cin>>fh;
+    cl=(fh-32)/1.8;
+    std::cout<<"\n temprature in celscius "<<cl;
+  }
+  else if (choice==2)
+  {
+    std::cout<<"\n enter temprature in celscius ";
+    std::cin>>cl;
+    fh=(cl*1.8)+32;
+    std::cout<<"\n temprature in fahrenheit "<<fh;
+  }
+  else 
+  {
+    std::cout<<"\n invalid choice!!! ";
+  }
+  return 0;
+}*/
+/*#include<iostream>
+int main()
+{
+  int n;
+  std::cout<<"\n enter the number ";
+  std::cin>>n;
+  if((n>=65)&&(n<=90))
+  {
+    std::cout<<"\n it is capital letter "<<n;
+  }
+  else if ((n>=97)&&(n<=122))
+  {
+  std::cout<<"\n it is small alphabet "<<n;
+  }
+  else if ((n>=48)&&(n<=57))
+  {
+  std::cout<<"\n it is number key "<<n;
+  }
+  else 
+  {
+    std::cout<<"\n it is an special key "<<n;
+  }
+}*/
+/*#include<iostream>
+int main()
+{
+  int n;
+  std::cout<<"\n enter the number ";
+  std::cin>>n;
+  if (n<0)
+  {
+    std::cout<<"\n It is a negative number "<<n;
+  }
+  else 
+  {
+    std::cout<<"\n It is a positive number "<<n;
+  }
+}*/
