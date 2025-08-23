4d01e94d02ae462f376994ea6a19b44fbe1206b8
Aryanparashara <aryanparashara01@gmail.com>
2025-08-23 11:24:50 +0530

    Create 22august2025.cpp

---
+++ 22august2025.cpp
@@ -0,0 +1,28 @@
+/*#include<math.h>
+#include<iostream>
+int main()
+{
+  int no,p,result;
+  std::cout<<"\n Enter the number ";
+  std::cin>>no;
+  std::cout<<"\n Enter its power ";
+  std::cin>>p;
+  result=pow(no,p);
+  std::cout<<"\n answer is "<<result;
+}*/
+/*#include<iostream>
+int add(int a,int b)
+{
+  int sum=0;
+  sum=a+b;
+  std::cout<<"\n the result is : "<<sum;
+  return 0;
+}
+int main()
+{
+  int a,b;
+  std::cout<<"\n enter two values: ";
+  std::cin>>a>>b;
+  add(a,b);
+  return 0;
+}*/
