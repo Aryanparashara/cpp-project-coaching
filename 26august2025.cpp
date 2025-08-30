7bb0c70988d232365acaa5d534637a967280d4f9
Aryanparashara <aryanparashara01@gmail.com>
2025-08-30 11:03:32 +0530

    Create 26august2025.cpp

---
+++ 26august2025.cpp
@@ -0,0 +1,19 @@
+/*#include<iostream>
+int simpleinterest(int x,int y,int z)
+{
+  int si;
+  si=(x*y*z)/100;
+  std::cout<<"\n si is "<<si;
+}
+int main()
+{
+  int p,r,t;
+  std::cout<<"\n enter the principle amount ";
+  std::cin>>p;
+  std::cout<<"\n enter the rate ";
+  std::cin>>r;
+  std::cout<<"\n enter the time ";
+  std::cin>>t;
+  simpleinterest(p,r,t);
+  return 0;
+}*/
