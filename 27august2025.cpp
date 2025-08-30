dbfcee01fdf32533887402e5db4ee4961b29cd57
Aryanparashara <aryanparashara01@gmail.com>
2025-08-30 11:03:57 +0530

    Create 27august2025.cpp

---
+++ 27august2025.cpp
@@ -0,0 +1,33 @@
+/*#include<iostream>
+int evenodd(int x)
+{
+  if(x%2==0)
+  std::cout<<"\n it is a even number ";
+  else 
+  std::cout<<"\n it is a odd number ";
+}
+int main()
+{
+  int x;
+  std::cout<<"\n enter the no. ";
+  std::cin>>x;
+  evenodd(x);
+  return 0;
+}*/
+/*#include<iostream>
+evenodd();
+int main()
+{
+  evenodd();
+}
+evenodd()
+{
+  int a;
+  std::cout<<"\n enter any number ";
+  std::cin>>a;
+  if(a%2==0)
+  std::cout<<"\n even ";
+  else 
+  std::cout<<"\n odd ";
+  return 0;
+}*/
