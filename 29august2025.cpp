ccad85b8c500e75e78e9936a81244cfe6246c0ba
Aryanparashara <aryanparashara01@gmail.com>
2025-09-01 12:05:56 +0530

    Create 29august2025.cpp

---
+++ 29august2025.cpp
@@ -0,0 +1,25 @@
+/*#include<iostream>
+int add(int,int);
+float average(int,int);
+int main()
+{
+  int n1,n2,sum;
+  float avg;
+  std::cout<<"\n Enter three numbers ";
+  std::cin>>n1>>n2;
+  std::cout<<" Number1 :"<<n1;
+  std::cout<<" Number2 :"<<n2;
+  sum=add(n1,n2);
+  avg=average(n1,n2);
+  std::cout<<"\n sum: "<<sum;
+  std::cout<<"\n Average: "<<avg;
+  return 0;
+}
+int add(int x,int y)
+{
+  return x+y;
+}
+float average(int x,int y)
+{
+  return((float)(x)+(float)(y))/2;
+}*/
