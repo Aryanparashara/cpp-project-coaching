ddc785d8e4eec35f709e259b679096395249f898
Aryanparashara <aryanparashara01@gmail.com>
2025-10-14 21:43:53 +0530

    Create 10oct2025.cpp

---
+++ 10oct2025.cpp
@@ -0,0 +1,42 @@
+/*#include<iostream>
+int main()
+{
+  int i,j,c[2][2],matrix1[2][2],matrix2[2][2];
+  std::cout<<"\n Enter the Elements : ";
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      std::cout<<"\n Enter elements "<<"["<<i<<"]"<<"["<<j<<"]";
+      std::cin>>matrix1[i][j];
+    }
+  }
+  std::cout<<"\n The Entered matrix 1 is : \n ";
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      std::cout<<matrix1[i][j];
+    }
+    std::cout<<"\n ";
+  }
+  std::cout<<"\n Enter the elements ";
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      std::cout<<"\n Enter elements "<<"["<<i<<"]"<<"["<<j<<"]";
+      std::cin>>matrix2[i][j];
+    }
+  }
+  std::cout<<"\n The entered matrix 2 is : \n ";
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      c[i][j]=matrix1[i][j]+matrix2[i][j];
+      std::cout<<"\n"<<c[i][j];
+    }
+    std::cout<<"\n ";
+  }
+}*/
