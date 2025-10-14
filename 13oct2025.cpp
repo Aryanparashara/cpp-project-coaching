3739b498a19e66210eded7acf1b60f9f64990ea1
Aryanparashara <aryanparashara01@gmail.com>
2025-10-14 21:45:03 +0530

    Create 13oct2025.cpp

---
+++ 13oct2025.cpp
@@ -0,0 +1,24 @@
+/*#include<iostream>
+int main()
+{
+  int array[100];
+  int i,n,x,flag=0;
+  std::cout<<"\n Enter size of an array ";
+  std::cin>>n;
+  std::cout<<"\n Enter the Elements ";
+  for(i=0;i<n;i++)
+  {
+    std::cin>>array[i];
+  }
+  std::cout<<"\n Enter the element you want to search ";
+  std::cin>>x;
+  for(i=0;i<n;i++)
+  {
+    if(x==array[i])
+    flag=1;
+  }
+  if(flag==1)
+  std::cout<<"\n Element found ";
+  else 
+  std::cout<<"\n Element not found ";
+}*/
