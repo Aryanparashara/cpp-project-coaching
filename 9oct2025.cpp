d4b79d5779a128651fbdcdfc77b1efb279e5c573
Aryanparashara <aryanparashara01@gmail.com>
2025-10-14 21:43:23 +0530

    Create 9oct2025.cpp

---
+++ 9oct2025.cpp
@@ -0,0 +1,24 @@
+// Matrix format 
+/*#include<iostream>
+int main()
+{
+  int i,j,matrix[2][2];
+  std::cout<<"\n Enter the elements ";
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      std::cout<<"\n Enter elements "<<"["<<i<<"]"<<"["<<j<<"]";
+      std::cin>>matrix[i][j];
+    }
+  }
+  std::cout<<"\n The Entered matrix is : \n ";
+  for(i=0;i<2;i++)
+  {
+    for(j=0;j<2;j++)
+    {
+      std::cout<<matrix[i][j];
+    }
+    printf("\n ");
+  }
+}*/
